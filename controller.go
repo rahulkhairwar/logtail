62b637fbd4e691c473f970adcbaf0cb335cee19b
Rahul Khairwar <rahulkhairwar@gmail.com>
2021-10-23 19:25:04 +0800

    Add GetLogs service

--- controller.go
+++ controller.go
@@ -16,6 +16,19 @@ func (l *logsController) SetupRoutes(r *mux.Router) {
 	r.HandleFunc("/logs", logsHandler).Name("logsHandler").Queries("pageSize", "{pageSize:[0-9]+}").Methods(http.MethodGet)
 }
 
+func (l *logsController) Get(r *http.Request) ([]string, error) {
+	ctx := r.Context()
+	// todo: fix.
+	pageSize := 0
+
+	res, err := l.svc.GetLogs(ctx, pageSize)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
 func NewLogsController(svc LogsService) *logsController {
 	return &logsController{
 		svc: svc,
